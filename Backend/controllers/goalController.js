d6792217a6ab5ecbbce9e24e207ed69059eb7c5a
rysacdalan <sacdalanryanchristian6404@gmail.com>
2022-04-15 18:57:04 +0800

    Add Middleware and install express async handler.

--- Backend/controllers/goalController.js
+++ Backend/controllers/goalController.js
@@ -1,30 +1,36 @@
+const asyncHandler = require("express-async-handler");
+
 //Description: Get all goals
 //Route: GET /api/goals
-const getGoals = (req, res) => {
+const getGoals = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "Get goals successfully" });
-};
+});
 
 //Description: Set a single goal
 //Route: POST /api/goals
-const setGoal = (req, res) => {
+const setGoal = asyncHandler(async (req, res) => {
+  if (!req.body.text) {
+    res.status(400);
+    throw new Error("Please add a text field");
+  }
   res.status(200).json({ message: "Set goals successfully" });
-};
+});
 
 //Description: Edit/Updating a single goal
 //Route: PUT /api/goals/:id
-const updateGoal = (req, res) => {
+const updateGoal = asyncHandler(async (req, res) => {
   res
     .status(200)
     .json({ message: `Update goals successfully for id: ${req.params.id}` });
-};
+});
 
 //Description: Delete a single goal
 //Route: DELETE /api/goals/:id
-const deleteGoal = (req, res) => {
+const deleteGoal = asyncHandler(async (req, res) => {
   res
     .status(200)
     .json({ message: `Goal for id: ${req.params.id} successfully deleted!` });
-};
+});
 
 module.exports = {
   getGoals,
--- Backend/server.js
+++ Backend/server.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
+const { errorHandler } = require("./middleware/errorMiddleware");
 const port = process.env.PORT || 5000;
 
 const app = express();
+
+//middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+//error handler
+app.use(errorHandler);
 
 app.use("/api/goals", require("./routes/goalRoutes"));
 
