2e745926b1f103d6f5dad95961ba5269d22e479c
rysacdalan <sacdalanryanchristian6404@gmail.com>
2022-05-03 14:36:27 +0800

    Login function.

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,11 +1,37 @@
 import { useState, useEffect } from "react"
 import {FaSignInAlt} from 'react-icons/fa'
+import {useSelector, useDispatch} from 'react-redux'
+import {useNavigate} from 'react-router-dom'
+import { toast } from "react-toastify"
+import { login, reset } from "../features/auth/authSlice"
+import Spinner from '../components/Spinner'
 
 function Login() {
   const [formData, setFormData] = useState({
     email: '',
     password: '',
   })
+
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+
+  const {user, isLoading, isError, isSuccess, message } = useSelector(
+    (state) => state.auth
+  )
+
+  useEffect(() => {
+    if(isError){
+      toast.error(message)
+    }
+
+    if(isSuccess || user){
+      navigate('/')
+    }
+
+    dispatch(reset())
+    
+  }, [user, isError, isSuccess, message, navigate, dispatch])
+
 
   const {email, password} = formData
 
@@ -18,6 +44,17 @@ function Login() {
 
   const onSubmit = (e) => {
     e.preventDefault()
+
+    const userData = {
+      email,
+      password,
+    }
+
+    dispatch(login(userData))
+  }
+
+  if(isLoading){
+    return <Spinner />
   }
 
   return (
